850ce4b953834966ab4fd3925e278335c46e3f6b
davidaguilar1 <100818462+davidaguilar1@users.noreply.github.com>
2025-09-25 16:08:51 -0400

    nice

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject Astoid;
+    public float spawnInterval = 2f; // time in between spawning 
+    private float timer = 0f; // tracking time 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > spawnInterval)
+        {
+            timer = 0f;
+            Instantiate(Astoid, transform.position, transform.rotation);
+        }
     }
 }
